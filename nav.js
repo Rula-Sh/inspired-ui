9096699453318190abb96246ae19697906da3c2d
Rula-Sh <1.rulashaban@gmail.com>
2026-03-31 17:48:55 +0300

    Update nav

--- nav.js
+++ nav.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navHTML = `
     <nav class="navbar">
-      <span class="title" id="home-title">
+      <span class="title">
         <a href="/#">Home</a>
       </span>
+      <span class="title">
+        <a href="#featured">Featured</a>
+      </span>
       <div class="container" id="carousels-container">
-        <span class="title" id="carousels-title">Carousels</span>
-        <ul id="carousels-list">
+        <span class="title">
+          <a href="#carousels">Carousels</a>
+        </span>
+        <ul>
           <a href="/carousels/3d-carousel/">
             <li>3D Carousel</li>
           </a>
           <a href="/carousels/draggable-carousel/">
-              <li>Draggable Carousel</li>
+            <li>Draggable Carousel</li>
           </a>
           <a href="/carousels/infinite-auto-scroll-carousel/">
-              <li>Infinite Auto Scroll Carousel</li>
+            <li>Infinite Auto Scroll Carousel</li>
           </a>
           <a href="/carousels/scroll-carousel/">
             <li>Scroll Carousel</li>
@@ -25,8 +30,10 @@ document.addEventListener("DOMContentLoaded", function () {
         </ul>
       </div>
       <div class="container" id="animations-container">
-        <span class="title">Animations</span>
-        <ul id="animations-list">
+        <span class="title">
+          <a href="#animations">Animations</a>
+        </span>
+        <ul>
           <a href="/animations/border-hover/">
             <li>Border Hover</li>
           </a>
@@ -43,18 +50,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // insert nav at the beginning of the body
   document.body.insertAdjacentHTML("afterbegin", navHTML);
+  
   const containers = document.getElementsByClassName("container");
-
   for (container of containers) {
     const title = container.childNodes[1];
     const list = container.childNodes[3];
     container.addEventListener("mouseover", () => {
       list.style.display = "block";
-      title.style.backgroundColor = "#0000008a";
+      title.childNodes[1].style.backgroundColor = "#0000008a";
+      title.childNodes[1].style.color = "white";
     });
     container.addEventListener("mouseout", () => {
       list.style.display = "none";
-      title.style.backgroundColor = "transparent";
+      title.childNodes[1].style.backgroundColor = "transparent";
+      title.childNodes[1].style.color = "#1e3c72";
     });
   }
 });
